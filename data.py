4cb8770c4df48925e3f168ef27bb4e80e280f439
ananya4231 <110167609+ananya4231@users.noreply.github.com>
2025-12-30 23:35:53 +0000

    Add files via upload

---
+++ data.py
@@ -0,0 +1,26 @@
+# data.py
+COLOURS = [
+    {"prompt": "Blue", "answer": "Bleu"},
+    {"prompt": "Green", "answer": "Vert"},
+    {"prompt": "Yellow", "answer": "Jaune"},
+    {"prompt": "Red", "answer": "Rouge"},
+    {"prompt": "White", "answer": "Blanc"},
+    {"prompt": "Black", "answer": "Noir"},
+    {"prompt": "Pink", "answer": "Rose"},
+    {"prompt": "Grey", "answer": "Gris"},
+    {"prompt": "Orange", "answer": "Orange"},
+    {"prompt": "Brown", "answer": "Marron"},
+]
+
+FRUITS = [
+    {"prompt": "Apple", "answer": "Pomme"},
+    {"prompt": "Banana", "answer": "Banane"},
+    {"prompt": "Strawberry", "answer": "Fraise"},
+    {"prompt": "Grape", "answer": "Raisin"},
+    {"prompt": "Pear", "answer": "Poire"},
+    {"prompt": "Peach", "answer": "Pêche"},
+    {"prompt": "Cherry", "answer": "Cerise"},
+    {"prompt": "Pineapple", "answer": "Ananas"},
+    {"prompt": "Lemon", "answer": "Citron"},
+    {"prompt": "Watermelon", "answer": "Pastèque"},
+]
